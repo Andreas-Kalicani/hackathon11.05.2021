11df5369da42fc117e0f67ef1d0ef5b2d86a72a7
Andreaskalicani <kaliqani7@gmail.com>
2021-05-11 14:47:56 +0200

    added background image component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import './App.css';
+import Background from "./components/Background";
 
 function App() {
   return (
     <div className="App">
-  
+    <Background/>
     </div>
   );
 }
---
+++ src/components/Background.js
@@ -0,0 +1,14 @@
+import React from "react"; 
+import "./Background.css"
+
+
+const Background = () => {
+    return (
+
+    <img className="background_image" src="./images/mars.jpg" />
+       
+    );
+}
+
+
+export default Background; 
