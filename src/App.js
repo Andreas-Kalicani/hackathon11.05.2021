d6d9f6c37ac7bcb94f5359fc0f9abe131b0436db
Andreaskalicani <kaliqani7@gmail.com>
2021-05-11 19:16:13 +0200

    Added point view where point components can be added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './App.css';
-import Background from "./components/Background";
+import Point from "./components/points";
 import Title from "./components/Title";
 import Mars from "./images/mars.jpg";
 
@@ -9,10 +9,11 @@ import Mars from "./images/mars.jpg";
 function App() {
   return (
     <div className="App">
-     <div style={{ backgroundImage: `url(${Mars})`, height:"100vh",  backgroundRepeat: "no-repeat", backgroundSize:"cover" }} >
+     <div className="flex" style={{ backgroundImage: `url(${Mars})`, height:"100vh",  backgroundRepeat: "no-repeat", backgroundSize:"cover" }} >
        <Title title={"Mars Scrum"}/>
-       
+       <Point/> 
     </div>
+    
     </div>
   );
 }
---
+++ src/components/points.js
@@ -0,0 +1,13 @@
+import React from "react"; 
+import "./Background.css"; 
+
+const Point = (props) => {
+    return (
+        <div className="point-container">
+        <h2 className="point-view">Points:</h2>
+        <h2 className="point-view">Award:</h2>
+        </div>
+    )
+}
+
+export default Point; 
